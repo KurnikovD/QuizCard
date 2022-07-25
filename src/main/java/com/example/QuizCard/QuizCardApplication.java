faa1443188a7aca24e6c14cd1c73c238d0042182
KurnikovD <34297950+KurnikovD@users.noreply.github.com>
2022-07-25 12:48:13 +0300

    initial commit

---
+++ src/main/java/com/example/QuizCard/QuizCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuizCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizCardApplication.class, args);
+	}
+
+}
