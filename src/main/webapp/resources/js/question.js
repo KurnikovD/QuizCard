4818577535cb7fd8b843d0edcbb6cd5e23f08088
KurnikovD <34297950+KurnikovD@users.noreply.github.com>
2022-07-25 12:55:22 +0300

    add js for button

---
+++ src/main/webapp/resources/js/question.js
@@ -0,0 +1,8 @@
+function showAnswer(){
+    var answerField = document.getElementById("answerField");
+    var answerButton = document.getElementById("answerButton");
+    var toDeskButton = document.getElementById("toDeskButton");
+    answerField.style.display = "block";
+    answerButton.style.display = "none";
+    toDeskButton.style.display = "block";
+}
